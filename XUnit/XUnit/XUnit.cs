8e65a1718b4b40c06902be6eab676c6c6c8eb09a
=Antony <guilhermecosta994@gmail.com>
2023-02-04 18:46:22 -0300

    Introduzindo a possibilidade de utilização do SetUp e utilizando o próprio framework de testes para se testar

--- XUnit/XUnit/XUnit.cs
+++ XUnit/XUnit/XUnit.cs
@@ -4,9 +4,7 @@ using System.Reflection;
 namespace XUnit
 {
     public class XUnit
-    {
-        public static object wasRun { get; private set; }
-
+    { 
         /*
             # Invoque o método teste
             Invoque setUp primeiro
@@ -18,12 +16,29 @@ namespace XUnit
 
         public static void Main() 
         {
-            var test = new WasRun("TestMethod");
-            XAssert.IsThruty(!test.TestMethodWasRun);
-            test.Run();
-            XAssert.IsThruty(test.TestMethodWasRun);
-            Console.WriteLine("Test Passed");
+            new TestCaseTest().Run();
+
+            var testSetUp = new WasRun("TestMethod");
+            testSetUp.Run();
+            XAssert.IsThruty(testSetUp.SetUpWasRan);
+            Console.WriteLine("Test - Run SetUp Passed");
             Console.ReadLine();
+        }
+    }
+
+    class TestCaseTest: TestCase<TestCaseTest> 
+    {
+        public TestCaseTest() : base(nameof(Should_Execute_A_Test))
+        {
+        }
+
+        public void Should_Execute_A_Test() 
+        {
+            var testRunTest = new WasRun("TestMethod");
+            XAssert.IsThruty(!testRunTest.TestMethodWasRun);
+            testRunTest.Run();
+            XAssert.IsThruty(testRunTest.TestMethodWasRun);
+            Console.WriteLine("Test - Run Test Passed");
         }
     }
 
@@ -32,7 +47,12 @@ namespace XUnit
         public WasRun(string testMethodName) : base(testMethodName) { }
 
         public bool TestMethodWasRun { get; internal set; }
+        public bool SetUpWasRan { get; internal set; }
 
+        public override void SetUp() 
+        {
+            SetUpWasRan = true;
+        }
 
         public void TestMethod()
         {
@@ -47,8 +67,12 @@ namespace XUnit
             TestMethodName = testMethodName;
         }
 
+        public virtual void SetUp() { }
+
         public void Run()
         {
+            SetUp();
+
             Type wasRunType = typeof(ClassThatImplementsTestCase);
             MethodInfo testMethod = wasRunType.GetMethod(TestMethodName);
             testMethod.Invoke(this, null);
@@ -60,8 +84,8 @@ namespace XUnit
     public static class XAssert 
     {
         public static void IsThruty(object value) 
-        { 
-            if(value is false) 
+        {
+            if (value is false) 
                 throw new ArgumentException(String.Format("Expected thruty but recieve {0}", value));
         }
     }
